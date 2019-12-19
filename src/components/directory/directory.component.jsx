e46635b4ff4c105ce85ad1d364a6b3c835de7799
Brandon Lee <brandonlee2543@hotmail.com>
2019-12-19 15:59:33 -0400

    Initial Commit, created HomePage, as well as used pre-defined styling for the most part, and begin including Routing capabilities.

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+
+import MenuItem from "../menu-item/menu-item.component";
+
+import "./directory.styles.scss";
+
+class Directory extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      sections: [
+        {
+          title: "hats",
+          imageUrl: "https://i.ibb.co/sFfF4vz/hats.png",
+          id: 1,
+          linkUrl: "hats"
+        },
+        {
+          title: "jackets",
+          imageUrl: "https://i.ibb.co/Yj27hhF/jackets.png",
+          id: 2
+        },
+        {
+          title: "shoes",
+          imageUrl: "https://i.ibb.co/sqmNZJf/shoes.png",
+          id: 3
+        },
+        {
+          title: "femme",
+          imageUrl: "https://i.ibb.co/sFXxpVC/femme.png",
+          id: 4,
+          size: "large"
+        },
+        {
+          title: "masc",
+          imageUrl: "https://i.ibb.co/DkKtV8L/masc.png",
+          id: 5,
+          size: "large"
+        }
+      ]
+    };
+  }
+
+  render() {
+    return (
+      <div className="directory-menu">
+        {this.state.sections.map(({ id, ...otherProps }) => (
+          <MenuItem key={id} {...otherProps} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Directory;

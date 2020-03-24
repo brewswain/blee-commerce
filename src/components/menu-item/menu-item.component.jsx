0617f1cd3365ee44d85504de5470c3d2618cecff
Brandon Lee <brandonlee2543@hotmail.com>
2020-03-24 14:06:45 -0400

    Began styling of Homepage, including updating images used for categories in our category reducer, as well as desktop view styling for menu-items

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -3,19 +3,22 @@ import { withRouter } from "react-router-dom";
 
 import "./menu-item.styles.scss";
 
-const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
-  <div
-    style={{
-      backgroundImage: `url(${imageUrl})`
-    }}
-    className={`${size} menu-item`}
-    onClick={() => history.push(`${match.url}${linkUrl}`)}
-  >
-    <div />
-    <div className="content-box">
-      <div className="content">
-        <h1 className="title">{title}</h1>
-        <span className="subtitle">SHOP NOW</span>
+const MenuItem = ({ title, imageUrl, history, linkUrl, match }) => (
+  <div className="menu-item-container">
+    <div
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }}
+      className={`${title} menu-item`}
+      onClick={() => history.push(`${match.url}${linkUrl}`)}
+    >
+      <div />
+      <div className="content-box">
+        <div className="content">
+          <h1 className="content-title">
+            {title.charAt(0).toUpperCase() + title.slice(1)}
+          </h1>
+        </div>
       </div>
     </div>
   </div>
--- src/redux/category/category.reducer.js
+++ src/redux/category/category.reducer.js
@@ -2,32 +2,32 @@ const INITIAL_STATE = {
   sections: [
     {
       title: "hats",
-      imageUrl: "https://i.ibb.co/sFfF4vz/hats.png",
+      imageUrl: "https://i.ibb.co/qF84jt8/test-image.png",
       id: 1,
       linkUrl: "shop/hats"
     },
     {
       title: "jackets",
-      imageUrl: "https://i.ibb.co/Yj27hhF/jackets.png",
+      imageUrl: "https://i.ibb.co/5TyFfKw/test-jacket.png",
       id: 2,
       linkUrl: "shop/jackets"
     },
     {
       title: "shoes",
-      imageUrl: "https://i.ibb.co/sqmNZJf/shoes.png",
+      imageUrl: "https://i.ibb.co/P5f4N5m/test-shoes.png",
       id: 3,
       linkUrl: "shop/shoes"
     },
     {
       title: "femme",
-      imageUrl: "https://i.ibb.co/sFXxpVC/femme.png",
+      imageUrl: "https://i.ibb.co/Q6dTZPV/test-femme.png",
       id: 4,
       size: "large",
       linkUrl: "shop/femme"
     },
     {
       title: "masc",
-      imageUrl: "https://i.ibb.co/DkKtV8L/masc.png",
+      imageUrl: "https://i.ibb.co/qYDRX1z/test-masc.png",
       id: 5,
       size: "large",
       linkUrl: "shop/masc"

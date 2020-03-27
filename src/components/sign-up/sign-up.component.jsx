430c09dbbd70df8c4e5fa8021712b5e7b4811a14
Brandon Lee <brandonlee2543@hotmail.com>
2020-03-27 10:58:57 -0400

    Made Authentication page mobile responsive

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -59,7 +59,7 @@ class SignUp extends React.Component {
 
     return (
       <div className="sign-up">
-        <h2 className="title">I do not have an account</h2>
+        <h2 className="title">I don't have an account</h2>
         <span>Sign up with your email and password</span>
 
         <form onSubmit={this.handleSubmit}>

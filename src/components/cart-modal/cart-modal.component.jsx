7d3d36f81b42d8295ed0cd891986bbf8571e21e9
Brandon Lee <brandonlee2543@hotmail.com>
2020-03-30 18:41:33 -0400

    Adjusted some minor styling to our Cart-modal, header, and app.js styling

--- src/components/cart-modal/cart-modal.component.jsx
+++ src/components/cart-modal/cart-modal.component.jsx
@@ -8,34 +8,42 @@ import CartItem from "../cart-item/cart-item.component";
 
 import { selectCartItems } from "../../redux/cart/cart.selectors";
 import { toggleVisibility } from "../../redux/cart/cart.actions";
+import { hideCart } from "../../redux/cart/cart.actions";
 
 import "./cart-modal.styles.scss";
 
-const CartModal = ({ history, cartItems, dispatch }) => (
-  <div className="cart-modal">
-    <div className="cart-items">
-      {cartItems.length ? (
-        cartItems.map(cartItem => (
-          <CartItem key={cartItem.id} item={cartItem} />
-        ))
-      ) : (
-        <span>Your cart is empty</span>
-      )}
+const CartModal = ({ history, cartItems, dispatch, isVisible }) => {
+  return (
+    <div className="cart-modal" onClick={console.log("hello")}>
+      <div className="cart-items">
+        {cartItems.length ? (
+          cartItems.map(cartItem => (
+            <CartItem key={cartItem.id} item={cartItem} />
+          ))
+        ) : (
+          <span>Your cart is empty</span>
+        )}
+      </div>
+      <CustomButton
+        className="custom-button cart-button"
+        onClick={() => {
+          history.push("/checkout");
+          dispatch(toggleVisibility());
+        }}
+      >
+        Checkout
+      </CustomButton>
     </div>
-    <CustomButton
-      className="custom-button cart-button"
-      onClick={() => {
-        history.push("/checkout");
-        dispatch(toggleVisibility());
-      }}
-    >
-      Checkout
-    </CustomButton>
-  </div>
-);
-
+  );
+};
 const mapStateToProps = createStructuredSelector({
   cartItems: selectCartItems
 });
 
-export default withRouter(connect(mapStateToProps)(CartModal));
+const mapDispatchToProps = dispatch => ({
+  hideCart: () => dispatch(hideCart())
+});
+
+export default withRouter(
+  connect(mapStateToProps, mapDispatchToProps)(CartModal)
+);
